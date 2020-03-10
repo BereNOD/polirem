fe7e50b2309c8d20f8c47ba5484aa1ea863b5866
jakson1405 <jakson1405@gmail.com>
2020-03-10 17:59:46 +0200

    Added lists component

--- src/components/Lists/index.jsx
+++ src/components/Lists/index.jsx
@@ -3,19 +3,34 @@ import _ from 'lodash';
 
 import "./styles.scss";
 
-const Lists = ({
-    title,
-    items
-}) => (
-    <React.Fragment>
-        <span className='list-title'>{title}</span>
-        <ul>
-            {_.map(items, item => <li className='list-item'>{item}</li>)}
-        </ul>
-    </React.Fragment>
-);
+// const Lists = ({
+//     title,
+//     items
+// }) => (
+//     <React.Fragment>
+//         <span className='list-title'>{title}</span>
+//         <ul>
+//             {_.map(items, item => <li className='list-item'>{item}</li>)}
+//         </ul>
+//     </React.Fragment>
+// );
 
 
 export default Lists;
 
 
+const Lists = {
+    title,
+    items
+};
+
+render = () => {
+    return(
+        <React.Fragment>
+            <span className='list-title'>{title}</span>
+            <ul>
+                {_.map(items, item => <li className='list-item'>{item}</li>)}
+            </ul>
+        </React.Fragment>
+    );
+};
