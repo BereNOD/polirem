3707f8b4728769863a5946de62ef598a1bfde466
smirnoffnew <smirnoffnew@gmail.com>
2020-03-14 15:08:29 +0200

    List fixxes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
+import List from "./../components/List";
+
 class HomePage extends React.Component {
+  state = {
+    list: []
+  };
+
+  handleClick = () => {
+    this.setState({
+      list: [...this.state.list, this.state.list.length]
+    });
+  };
+
   render = () => {
-    return <h1>HomePage</h1>;
+    return (
+      <>
+        <button onClick={this.handleClick}>Add</button>
+        <List title="HomePage" items={[this.state.list.length]} />
+      </>
+    );
   };
 }
 
