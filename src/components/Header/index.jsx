bf9552fa245ad46f1a37733dc5d028e1e89aabc3
smirnoffnew <smirnoffnew@gmail.com>
2020-03-14 16:01:55 +0200

    Added cart

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import _ from "lodash";
 import Navigation from "./../Navigation";
 import { Link } from "react-router-dom";
+import { CartContext } from "./../../Cart";
 
 import "./styles.scss";
 
@@ -12,71 +14,78 @@ const data = {
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <div className="header">
-          <div className="header__top">
-            <div className="logo">
-              <Link to="/">
-                <img src="image/logo-final-1.png" alt="logo" />
-              </Link>
+      <CartContext.Consumer>
+        {({ list }) => (
+          <header>
+            <div className="header">
+              <div className="header__top">
+                <div className="logo">
+                  <Link to="/">
+                    <img src="image/logo-final-1.png" alt="logo" />
+                  </Link>
+                </div>
+                <div className="contacts">
+                  <div className="contacts-numder">
+                    <a href="#" className="number">
+                      <img src="image/phone-icon.png" alt="phone" />
+                      {data.telephone}
+                    </a>
+                  </div>
+                  <div className="contacts-address">
+                    <a href="#" className="address">
+                      <img src="image/location-icon.png" alt="location" />
+                      {data.country}
+                    </a>
+                  </div>
+                </div>
+                <div className="header__search">
+                  <input type="text" placeholder="Поиск по товарам" />
+                  <div className="search">
+                    <img
+                      className="search-icon"
+                      src="image/search-icon.png"
+                      alt="search"
+                    />
+                    <a href="#">Найти</a>
+                  </div>
+                </div>
+                <Link className="private__cabinet" to="/cabinet">
+                  <div class="header__private__cabinet">
+                    <div class="user">
+                      <img
+                        class="user__icon"
+                        src="image/user-icon.png"
+                        alt="user"
+                      />
+                    </div>
+                    <div class="private">
+                      <a class="private__cabinet">Личный кабинет</a>
+                    </div>
+                  </div>
+                </Link>
+                <Link className="cart" to="/cart">
+                  <div className="header__cart">
+                    <div className="cart">
+                      <img
+                        className="cart__icon"
+                        src="image/cart-icon.png"
+                        alt="cart"
+                      />
+                      {_.size(list) > 0 ? (
+                        <span className="cart__count">{_.size(list)}</span>
+                      ) : null}
+                    </div>
+                    <div className="cart">
+                      <a className="cart__link">Корзина</a>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+              <Navigation />
             </div>
-            <div className="contacts">
-              <div className="contacts-numder">
-                <a href="#" className="number">
-                  <img src="image/phone-icon.png" alt="phone" />
-                  {data.telephone}
-                </a>
-              </div>
-              <div className="contacts-address">
-                <a href="#" className="address">
-                  <img src="image/location-icon.png" alt="location" />
-                  {data.country}
-                </a>
-              </div>
-            </div>
-            <div className="header__search">
-              <input type="text" placeholder="Поиск по товарам" />
-              <div className="search">
-                <img
-                  className="search-icon"
-                  src="image/search-icon.png"
-                  alt="search"
-                />
-                <a href="#">Найти</a>
-              </div>
-            </div>
-            <Link className="private__cabinet" to="/cabinet">
-              <div class="header__private__cabinet">
-                <div class="user">
-                  <img
-                    class="user__icon"
-                    src="image/user-icon.png"
-                    alt="user"
-                  />
-                </div>
-                <div class="private">
-                  <a class="private__cabinet">Личный кабинет</a>
-                </div>
-              </div>
-            </Link>
-            <Link className="cart" to="/cart">
-              <div className="header__cart">
-                <div className="cart">
-                  <img
-                    className="cart__icon"
-                    src="image/cart-icon.png"
-                    alt="cart"
-                  />
-                </div>
-                <div className="cart">
-                  <a className="cart__link">Корзина</a>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <Navigation />
-        </div>
-      </header>
+          </header>
+        )}
+      </CartContext.Consumer>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 import Router from "./Router";
 import "./styles.css";
 import "./styles.scss";
+import Cart from "./Cart";
 
-ReactDOM.render(<Router />, document.getElementById("app"));
+ReactDOM.render(
+  <Cart>
+    <Router />
+  </Cart>,
+  document.getElementById("app")
+);
